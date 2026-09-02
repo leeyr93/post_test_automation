fd4dc848f12edcfbe9edff006be9969f92341f21
leeyr <youlanlee3@gmail.com>
2026-09-02 22:42:22 +0900

    refactor: replace expect assertion with wait_for_url in auth

--- utils/auth.py
+++ utils/auth.py
@@ -1,4 +1,3 @@
-from playwright.sync_api import expect
 from pages.login_page import LoginPage
 from utils import url
 
@@ -6,4 +5,4 @@ def login(page, user_id, password):
     login_page = LoginPage(page)
     login_page.open()
     login_page.login(user_id, password)
-    expect(page).to_have_url(url.URL_BOARD_LIST)
+    page.wait_for_url(url.URL_BOARD_LIST)
